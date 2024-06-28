2605333c488fffd297f37f9e85a1edd6d85d1ef8
Sony666999 <stepanovasa22@yandex.ru>
2024-06-28 21:58:10 +0300

    переделываем под задание

--- raboch.py
+++ raboch.py
@@ -1,35 +1,42 @@
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QTextEdit, QPushButton, QTableWidget, QTableWidgetItem
-
+import sys
 from library import *
 
-import sys
-
-app = QApplication([])
+app = QtWidgets.QApplication([])
 win = uic.loadUi("рабочие.ui")
 
-gr = Group()
+gr = grup()
 gr.read_data_from_file("text.txt")
 print("!!!", gr.count)
 
-win.tableWidget = QTableWidget()
+sti = QtWidgets.QStandardItemModel(parent=win)
+win.tableWidget = QtWidgets.QModelIndex()
+win.label.setAlignment(QtCore.Qt.AlignHCenter)
 
 def btn_load_table():
+    win.tableWidget.setRowCount(gr.count)
     row = 0
     for x in gr.a:
-        win.tableWidget.setItem(row, 0, QTableWidgetItem(gr.a[x].fam + ' ' + gr.a[x].name + ' ' + gr.a[x].otchestvo))
+        print(1)
+
+        win.tableWidget.setItem(row, 0, QTableWidgetItem(gr.a[x].fam+' '+gr.a[x].name+' '+gr.a[x].otchestvo))
         win.tableWidget.setItem(row, 1, QTableWidgetItem(str(gr.a[x].year)))
         win.tableWidget.setItem(row, 2, QTableWidgetItem(gr.a[x].city))
-        win.tableWidget.setItem(row, 3, QTableWidgetItem(str(gr.a[x].sbinf)))
-        win.tableWidget.setItem(row, 4, QTableWidgetItem(str(gr.a[x].sbmath)))
+        win.tableWidget.setItem(row, 3, QTableWidgetItem(str(gr.a[x].sb_inf)))
+        win.tableWidget.setItem(row, 4, QTableWidgetItem(str(gr.a[x].sb_math))
         row += 1
 
 def btn_append_person():
     str_person = win.lineEdit.text()
     gr.append_person(str_person)
+    win.tableWidget.clear()
+    btn_load_table()
 
 win.pushButton.clicked.connect(btn_load_table)
 print(gr.a[0].get_person_for_table())
+
+win.pushButton_3.clicked.connect(btn_append_person)
 
 win.show()
 sys.exit(app.exec())
